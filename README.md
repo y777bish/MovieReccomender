2d017ce936097fa88f196da6f2a83e617387a174
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2023-06-02 22:19:15 +0200

    first readme and skeleton code for algorithm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MovieReccomender
+
+This is a program which is aimed to help you reccomend which movie you should watch next! It is based on soft sets and uses libraries such as Pandas, Seaborn (for data analysis) or Flask (for data visualisation in our web application).
--- nieinterere.py
+++ nieinterere.py
@@ -1,9 +1,65 @@
 # In[1]:
 
-import numpy as np
-import random as rd
-import seaborn as sns
-import math
-import pandas as pd #przetwarzanie danych
-import statistics
+import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import seaborn as sns
+import numpy as np
+import json
+import warnings
+warnings.filterwarnings('ignore')
+import base64
+import io
+from matplotlib.pyplot import imread
+import codecs
+from IPython.display import HTML
+
+# %%
+
+movies = pd.read_csv('titles.csv')
+credits = pd.read_csv('credits.csv')
+
+# %%
+
+movies.head()
+# %%
+
+movies['genres'] = movies['genres'].str.strip('[]').str.replace(' ','').str.replace("'",'')
+movies['genres'] = movies['genres'].str.split(',')
+plt.subplots(figsize=(12,10))
+list1 = []
+for i in movies['genres']:
+    list1.extend(i)
+ax = pd.Series(list1).value_counts()[:10].sort_values(ascending=True).plot.barh(width=0.9,color=sns.color_palette('hls',10))
+for i, v in enumerate(pd.Series(list1).value_counts()[:10].sort_values(ascending=True).values): 
+    ax.text(.8, i, v,fontsize=12,color='white',weight='bold')
+plt.title('Top Genres')
+plt.show()
+
+
+# %%
+
+for i,j in zip(movies['genres'],movies.index):
+    list2=[]
+    list2=i
+    list2.sort()
+    movies.loc[j,'genres']=str(list2)
+movies['genres'] = movies['genres'].str.strip('[]').str.replace(' ','').str.replace("'",'')
+movies['genres'] = movies['genres'].str.split(',')
+
+
+# %%
+
+genreList = []
+for index, row in movies.iterrows():
+    genres = row["genres"]
+    
+    for genre in genres:
+        if genre not in genreList:
+            genreList.append(genre)
+genreList[:10] #now we have a list with unique genres
+# %%
+
+movies['genres_bin'] = movies['genres'].apply(lambda x: binary(x))
+movies['genres_bin'].head()
+# %%
